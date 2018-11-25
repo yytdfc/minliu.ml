2f01f9a03eeaf319f9f8977d4ca7f805afd5d0f8
yytdfc <fuchen@foxmail.com>
2018-11-25 16:03:59 +0800

    Updates

---
+++ precache-manifest.1dc2f517ebf30fa9174899318fdae751.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1680be9a1a247baf26d8",
+    "url": "/minliu.ml/static/css/main.510d301a.chunk.css"
+  },
+  {
+    "revision": "1680be9a1a247baf26d8",
+    "url": "/minliu.ml/static/js/main.1680be9a.chunk.js"
+  },
+  {
+    "revision": "8a0e018449a2e303b7d1",
+    "url": "/minliu.ml/static/js/1.8a0e0184.chunk.js"
+  },
+  {
+    "revision": "a8659ecabc9cdbda497c",
+    "url": "/minliu.ml/static/js/runtime~main.a8659eca.js"
+  },
+  {
+    "revision": "9c7352f0924edd3af6be1352c6ffc023",
+    "url": "/minliu.ml/static/media/flower.9c7352f0.svg"
+  },
+  {
+    "revision": "3a35854dd79b09e4ff1e32e2f4fe4e7a",
+    "url": "/minliu.ml/static/media/happybirthday.3a35854d.mp3"
+  },
+  {
+    "revision": "616bdbd2b77e5f64fb770b8df4089a1f",
+    "url": "/minliu.ml/index.html"
+  }
+];
